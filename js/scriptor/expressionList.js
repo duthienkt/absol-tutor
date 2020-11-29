218a95aa3aedb1e2a9028d023ea055fbf81a956c
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-11-29 15:03:17 +0700

    + expressionList

---
+++ js/scriptor/expressionList.js
@@ -0,0 +1,27 @@
+import Explain from './Explain';
+import Timeout from './Timeout';
+import PressAnyKey from './PressAnyKey';
+import ClickAnyWhere from './ClickAnyWhere';
+import Or from './Or';
+import And from './And';
+import Earliest from './Earliest';
+import Latest from './Latest';
+import SnackBar from './SnackBar';
+import Appear from "./Appear";
+
+var commandList = [
+    Explain,
+    Timeout,
+    PressAnyKey,
+    ClickAnyWhere,
+    Or,
+    And,
+    Earliest,
+    Latest,
+    SnackBar,
+    Appear
+];
+
+
+export default commandList;
+
