f7026dae7309a2b736c712339475f8cee4ef50bb
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-11-29 15:03:39 +0700

    + runScriptFromUrl

---
+++ js/runScriptFromUrl.js
@@ -0,0 +1,11 @@
+import Tutor from "./Tutor";
+
+export default function runScriptFromUrl(url, elt) {
+    fetch(url).then(function (res) {
+        return res.text();
+    }).then(function (text) {
+        elt = elt || document.body;
+        var tutor = new Tutor(elt, text);
+        return tutor.exec();
+    });
+};
