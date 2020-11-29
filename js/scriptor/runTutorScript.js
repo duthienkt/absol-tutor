6f4cdc1eb0315422b55389c3d3b606ff0647fa8c
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-11-29 14:49:10 +0700

    - remove test

--- js/scriptor/runTutorScript.js
+++
@@ -1,7 +0,0 @@
-import getNodeTree from "../util/getNodeTree";
-
-export default function runTutorScript(rootElt, script, option){
-    rootElt = rootElt || document.body;
-    var nodeTree = getNodeTree(rootElt);
-    console.log(nodeTree)
-}
