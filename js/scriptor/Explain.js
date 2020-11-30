706c5dcbbbefed82749da2729aa53239d37b0f32
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-11-30 16:53:38 +0700

    - Explain exec, text

--- js/scriptor/Explain.js
+++ js/scriptor/Explain.js
@@ -3,6 +3,7 @@ import BaseCommand from './BaseCommand';
 import OOP from 'absol/src/HTML5/OOP';
 import {_} from '../dom/Core';
 import ToolTip from "absol-acomp/js/Tooltip";
+import wrapAsync from "../util/wrapAsync";
 
 
 /***
@@ -15,28 +16,30 @@ function Explain() {
 OOP.mixClass(Explain, BaseCommand);
 
 Explain.prototype.exec = function () {
-    var tutor = this.tutor;
-    if (!this.$puncturedModal.isDescendantOf(document.body)) {
-        this.$puncturedModal.addTo(document.body);
-    }
-    if (!this.$transparentModal.isDescendantOf(document.body)) {
-        this.$transparentModal.addTo(document.body);
-    }
+    return wrapAsync(this.args.text).then(function (text) {
+        if (!this.$puncturedModal.isDescendantOf(document.body)) {
+            this.$puncturedModal.addTo(document.body);
+        }
+        if (!this.$transparentModal.isDescendantOf(document.body)) {
+            this.$transparentModal.addTo(document.body);
+        }
 
-    this.$puncturedModal.removeStyle('visibility', 'hidden');
-    var contentElt = _({
-        class:'atr-explain-text',
-       child: {text: this.args.text}
-    });
-    var targetElt = $(this.tutor.findNode(this.args.eltPath));
-    var token = ToolTip.show(targetElt, contentElt, 'top');
-    this.$puncturedModal.follow(targetElt);
+        this.$puncturedModal.removeStyle('visibility', 'hidden');
+        var contentElt = _({
+            class: 'atr-explain-text',
+            child: { text: text }
+        });
+        var targetElt = $(this.tutor.findNode(this.args.eltPath));
+        var token = ToolTip.show(targetElt, contentElt, 'auto');
+        this.$puncturedModal.follow(targetElt);
 
-    return this.args.until.exec().then(function () {
-        this.$puncturedModal.addStyle('visibility', 'hidden');
-        this.$transparentModal.remove()
-        ToolTip.closeTooltip(token);
+        return this.args.until.exec().then(function () {
+            this.$puncturedModal.addStyle('visibility', 'hidden');
+            this.$transparentModal.remove();
+            ToolTip.closeTooltip(token);
+        }.bind(this));
     }.bind(this));
+
 };
 
 
@@ -45,5 +48,6 @@ Explain.attachEnv = function (tutor, env) {
         return new Explain(tutor, { eltPath: eltPath, text: text, until: until });
     };
 };
+
 
 export default Explain;
