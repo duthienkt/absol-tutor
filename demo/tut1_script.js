2fae2b38fb7792ef63142424fda8407811060043
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-01 18:44:22 +0700

    - demo

--- demo/tut1_script.js
+++ demo/tut1_script.js
@@ -5,24 +5,25 @@ function LATEST(...tringer: Trigger): Trigger;
 function EARLIEST(...tringer: Trigger): Trigger;
 */
 
+// DECLARE('s1', 'Đây là form đăng nhập');
+//
+// EXPLAIN('login_form', VAR('s1'),
+//     TIME_OUT(2000)
+// );
+//
+// ASSIGN('s1', 'Tên đăng nhập ở đây');
+// EXPLAIN('login_user_name', VAR('s1'),
+//     TIME_OUT(2000)
+// );
+// EXPLAIN('login_password', 'Ô  này nhập mật khẩu nè',
+//     TIME_OUT(2000)
+// );
+// EXPLAIN('forgot_password', 'Nếu bạn quên mật khẩu',
+//     TIME_OUT(2000)
+// );
+//
+// EXPLAIN(QUERY_SELECTOR('.submitBtn'), 'Nút đăng nhập',
+//     TIME_OUT(2000)
+// );
 
-// SNACK_BAR("Xin chào", EARLIEST(TIME_OUT(1000), TIME_OUT(500)));
-// SNACK_BAR("Hôm nay tôi sẽ hướng dẫn cho  bạn", LATEST(TIME_OUT(1000), TIME_OUT(500)));
-// SNACK_BAR("Bạn đã sẵn sàng chưa? Nhấn phím bất kì để tiếp tục",  EARLIEST(PRESS_ANY_KEY, CLICK_ANY_WHERE));
-// SNACK_BAR("3", TIME_OUT(500));
-// SNACK_BAR("2", TIME_OUT(500));
-// SNACK_BAR("1", TIME_OUT(500));
-// SNACK_BAR("Bắt đầu", TIME_OUT(100));
-EXPLAIN('login_form', 'Đây là form đăng nhập',
-    TIME_OUT(2000)
-);
-
-EXPLAIN('login_user_name', 'Tên đăng nhập ở đây',
-    TIME_OUT(2000)
-);
-EXPLAIN('login_password', 'Ô  này nhập mật khẩu nè',
-    TIME_OUT(2000)
-);
-EXPLAIN('forgot_password', 'Nếu bạn quên mật khẩu',
-    TIME_OUT(2000)
-);
+USER_CLICK('close_btn', "Nhấn vào đây để thoát");
---
+++ js/scriptor/type.js
@@ -0,0 +1,66 @@
+/***
+ * @exports window
+ * @global
+ * @function
+ * @name EXPLAIN
+ * @param eltPath
+ * @param text
+ * @param until
+ * @returns {Explain}
+ */
+
+/***
+ * @global
+ * @function
+ * @name TIME_OUT
+ * @param {number} millis
+ * @returns {Timeout}
+ */
+
+/***
+ * @global
+ * @function
+ * @name DECLARE
+ * @param {string} name
+ * @param {BaseCommand} initValue
+ * @returns {Declare}
+ */
+
+/***
+ * @global
+ * @function
+ * @name ASSIGN
+ * @param {string} name
+ * @param {BaseCommand} value
+ * @returns {Assign}
+ */
+
+/***
+ * @global
+ * @function
+ * @name VAR
+ * @param {string} name
+ * @returns {Var}
+ */
+
+/***
+ * @global
+ * @function
+ * @name QUERY_SELECTOR
+ * @param {string} query
+ * @returns {QuerySelector}
+ */
+
+/***
+ * @global
+ * @function
+ * @name USER_CLICK
+ * @param {string|QuerySelector} query
+ * @param {string|BaseCommand} message
+ * @returns {UserClick}
+ */
+
+
+
+
+
