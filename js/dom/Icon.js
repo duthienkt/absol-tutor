21365b8c3b40efc82c3c3497cdb91d861fbcc14a
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-07 18:29:02 +0700

    -  icon

--- js/dom/Icon.js
+++ js/dom/Icon.js
@@ -2,7 +2,9 @@ import '../../css/clickico.css';
 import '../../css/scrollbarico.css';
 import clickTpl from '../../asset/icon/click.tpl';
 import scrollBarTpl from '../../asset/icon/scrollbar.tpl';
+import tutorTpl from '../../asset/icon/tutor.tpl';
 import {_} from "./Core";
 
 export var ClickIco = _(clickTpl);
 export var ScrollBarIco = _(scrollBarTpl);
+export var TutorIco = _(tutorTpl);
