77906edfb5789fc9429b2e2cf5b246edc9bd5983
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-08 18:04:48 +0700

    - export

--- dev.js
+++ dev.js
@@ -2,9 +2,20 @@ import install from "./js/dom/install";
 import "absol/src/absol";
 import 'absol-acomp/dev';
 import 'absol-brace/dev';
-import  tutor from '.';
+import tutor from '.';
+import FlagManager from "./js/app/FlagManager";
+import Dom from "absol/src/HTML5/Dom";
+import TutorMaster from "./js/app/TutorMaster";
+
 install(absol.coreDom);
-
-
 absol.tutor = tutor;
+absol.FlagManager = FlagManager;
 
+Dom.documentReady.then(function () {
+    if (window.FLAG_MANAGER_STARTUP && window.self === window.top)
+        FlagManager.start();
+    if (window.TUTOR_STARTUP && window.self === window.top){
+        var tutorMaster = new TutorMaster();
+        tutorMaster.start();
+    }
+});
--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 import Tutor from './Tutor';
 import runScriptFromUrl from "./runScriptFromUrl";
+import SplitEditor from "./app/SplitEditor";
+import TutorMaster from "./app/TutorMaster";
+import FlagManager from "./app/FlagManager";
 
 export default {
+    FlagManager: FlagManager,
     Tutor: Tutor,
-    runScriptFromUrl: runScriptFromUrl
+    runScriptFromUrl: runScriptFromUrl,
+    SplitEditor: SplitEditor,
+    TutorMaster: TutorMaster
 }
