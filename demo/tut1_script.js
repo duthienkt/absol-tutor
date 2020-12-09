01592a14f207920d71f0e53d0c9a88252ed142ce
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-09 16:03:27 +0700

    - demo

--- demo/tut1_script.js
+++ demo/tut1_script.js
@@ -1,3 +1,4 @@
+USER_SELECT_MENU($('selecttreemenu'), 4373, 'Chọn "Dự Án & KỸ THUẬT"', 'Chọn đúng bộ phận "Dự Án & KỸ THUẬT"',"Có thể tìm kiếm để nhanh hơn");
 USER_CHECKBOX($('checkboxinput'), true, "Tick vào checkbox để hiện sách nhân viên", "Checkbox này phải được bật");
 // USER_INPUT_TEXT($('input[placeholder="type here to search..."]'), /.+/, "Nhập đoạn chuỗi vào đây để tìm kiếm", "Không được để trống")
 
