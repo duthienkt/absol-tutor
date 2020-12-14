59e14a50a72a007153309df6845693081d9fc489
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-14 12:45:50 +0700

    + ShowToastMessage

---
+++ js/scriptor/ShowToastMessage.js
@@ -0,0 +1,60 @@
+import BaseCommand from "./BaseCommand";
+import OOP from "absol/src/HTML5/OOP";
+import wrapAsync from "../util/wrapAsync";
+import Toast from "absol-acomp/js/Toast";
+
+/***
+ * @extends BaseCommand
+ * @constructor
+ */
+function ShowToastMessage() {
+    BaseCommand.apply(this, arguments);
+}
+
+OOP.mixClass(ShowToastMessage, BaseCommand);
+
+ShowToastMessage.prototype.exec = function () {
+    var thisC = this;
+    console.log(this.args)
+    return Promise.all([
+        wrapAsync(this.args.title),
+        wrapAsync(this.args.text),
+        wrapAsync(this.args.disappearTimeout),
+        wrapAsync(this.args.variant)
+    ]).then(function (args) {
+        var title = args[0];
+        var text = args[1];
+        var timeout = args[2];
+        var variant = args[3];
+        Toast.make({
+            props: {
+                htitle: title,
+                message: text,
+                disappearTimeout: timeout,
+                variant: variant
+            }
+        });
+        return wrapAsync(thisC.args.until);
+    });
+};
+
+ShowToastMessage.attachEnv = function (tutor, env) {
+    env.TOAST_MESSAGE = function (title, text, disappearTimeout, until, variant) {
+        console.log(variant)
+        return new ShowToastMessage(tutor, {
+            title: title,
+            text: text,
+            disappearTimeout: disappearTimeout || 0,
+            until: until,
+            variant: variant
+        })
+    };
+};
+
+
+export default ShowToastMessage;
+
+
+
+
+
