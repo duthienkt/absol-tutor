5434c9e67948711388b8de1d1c23684b1bd12e04
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-14 14:56:10 +0700

    - sample

--- demo/tut1_script.js
+++ demo/tut1_script.js
@@ -1,6 +1,6 @@
-USER_SELECT_MENU($('selecttreemenu'), 4373, 'Chọn "Dự Án & KỸ THUẬT"', 'Chọn đúng bộ phận "Dự Án & KỸ THUẬT"',"Có thể tìm kiếm để nhanh hơn");
+USER_SELECT_MENU($('selecttreemenu'), 4373,
+    'Trong menu bộ phận, chọn "Dự Án & KỸ THUẬT"', 'Chọn đúng bộ phận "Dự Án & KỸ THUẬT"', "Có thể tìm kiếm để nhanh hơn");
 USER_CHECKBOX($('checkboxinput'), true, "Tick vào checkbox để hiện sách nhân viên", "Checkbox này phải được bật");
-// USER_INPUT_TEXT($('input[placeholder="type here to search..."]'), /.+/, "Nhập đoạn chuỗi vào đây để tìm kiếm", "Không được để trống")
+USER_INPUT_TEXT($('input[placeholder="type here to search..."]'), /.+/, "Nhập đoạn chuỗi vào ô tìm kiếm", "Không được để trống")
 
-
-SNACK_BAR("Bạn đã hoàn thành bài hướng dẫn", TIME_OUT(100));
+TOAST_MESSAGE("Congratulations!", "Bạn đã hoàn thành bài hướng dẫn", 10000 ,TIME_OUT(100), 'success');
