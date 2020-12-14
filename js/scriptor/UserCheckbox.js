d0831ad9fbb5841fb5793e7aa39599e3ec5e77f5
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-14 12:48:03 +0700

    - use Toast

--- js/scriptor/UserCheckbox.js
+++ js/scriptor/UserCheckbox.js
@@ -27,10 +27,6 @@ UserCheckbox.prototype.exec = function () {
         var checked = args[1];
         var message = args[2];
         var wrongMessage = args[3];
-        var messageElt = _({
-            class: 'atr-explain-text',
-            child: { text: message }
-        });
 
         var wrongMessageElt = wrongMessage && _({
             class: 'atr-explain-text',
@@ -47,7 +43,7 @@ UserCheckbox.prototype.exec = function () {
         }
 
         thisC.onlyInteractWith(elt, onInteractOut);
-        tooltipToken = ToolTip.show(elt, messageElt, 'auto');
+        thisC.showToast(message);
 
         return new Promise(function (resolve) {
             function onChange() {
