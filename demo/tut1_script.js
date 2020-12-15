9d6381643719dd18bd3ed39537852c16eef6f9b3
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-15 18:22:03 +0700

    - demo

--- demo/tut1_script.js
+++ demo/tut1_script.js
@@ -1,6 +1,23 @@
-USER_SELECT_MENU($('selecttreemenu'), 4373,
-    'Trong menu bộ phận, chọn "Dự Án & KỸ THUẬT"', 'Chọn đúng bộ phận "Dự Án & KỸ THUẬT"', "Có thể tìm kiếm để nhanh hơn");
-USER_CHECKBOX($('checkboxinput'), true, "Tick vào checkbox để hiện sách nhân viên", "Checkbox này phải được bật");
-USER_INPUT_TEXT($('input[placeholder="type here to search..."]'), /.+/, "Nhập đoạn chuỗi vào ô tìm kiếm", "Không được để trống")
+var index = 0;
+var message = "Bạn đã hoàn thành bài hướng dẫn";
+var n = 2;
+function haha(){
+    for (var i = 0; i < n; ++i){
+        showToastMessage("Congratulations!", '['+(index++)+']'+message, 3000 , untilTimeout(100), 'success');
+        delay(1000);
+    }
+}
 
-TOAST_MESSAGE("Congratulations!", "Bạn đã hoàn thành bài hướng dẫn", 10000 ,TIME_OUT(100), 'success');
+
+// TOAST_MESSAGE("Congratulations!", "Bạn đã hoàn thành bài hướng dẫn", 10000 ,TIME_OUT(100), 'success');
+
+
+userSelectMenu($('selecttreemenu'), 4373,
+    'Trong menu bộ phận, chọn "Dự Án & KỸ THUẬT"', 'Chọn đúng bộ phận "Dự Án & KỸ THUẬT"',"Có thể tìm kiếm để nhanh hơn");
+userCheckbox($('checkboxinput'), true, "Tick vào checkbox để hiện sách nhân viên", "Checkbox này phải được bật");
+userInputText($('input[placeholder="type here to search..."]'), /.+/,
+    "Nhập đoạn chuỗi vào ô tìm kiếm", "Không được để trống");
+
+var inputelt = $('input[value="có sẵn value"]');
+if (showToastMessage("Text input", 'Giá trị là '+ inputelt.placeholder, 3000, untilTimeout(100)));
+haha();
