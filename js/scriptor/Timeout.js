8eb2ee46cf4dd6a4efd6f794fd43424e753a03fb
duthienkt <blueskythien2010@live.com>
2020-12-15 21:57:35 +0700

    - timeout, delay

--- js/scriptor/Timeout.js
+++ js/scriptor/Timeout.js
@@ -7,17 +7,44 @@ import FunctionKeyManager from "./FunctionNameManager";
  */
 function Timeout() {
     BaseCommand.apply(this, arguments);
+    this._timeoutId = -1;
+    this._rejectCb = null;
 }
 
 OOP.mixClass(Timeout, BaseCommand);
 
+
+Timeout.prototype.exec = function () {
+    var thisC = this;
+    if (thisC._timeoutId > 0){
+        throw  new Error("Trigger TIMEOUT is not finish before started again!");
+    }
+    return new Promise(function (resolve, reject) {
+        thisC._timeoutId = setTimeout(function () {
+            thisC._rejectCb = null;
+            thisC._timeoutId = -1;
+            resolve();
+        }, thisC.args.millis);
+        thisC._rejectCb = reject;
+    });
+};
+
+Timeout.prototype.cancel = function () {
+    if (this._timeoutId > 0) {
+        clearTimeout(this._timeoutId);
+        this._timeoutId = -1;
+    }
+    if (this._rejectCb) {
+        this._rejectCb();
+        this._rejectCb = null;
+    }
+};
+
 Timeout.attachEnv = function (tutor, env) {
-    env.untilTimeout = function (millis) {
-        return function () {
-            return new Promise(function (resolve) {
-                setTimeout(resolve, millis);
-            });
-        }
+    env.TIME_OUT = function (millis) {
+        return new Timeout(tutor, {
+            millis: millis
+        });
     };
     env.delay = function (millis) {
         return new Promise(function (resolve) {
@@ -26,7 +53,7 @@ Timeout.attachEnv = function (tutor, env) {
     }
 };
 
-FunctionKeyManager.addSync('untilTimeout')
-    .addSync('delay');
+FunctionKeyManager.addSync('TIME_OUT')
+    .addAsync('delay');
 
 export default Timeout;
--- js/scriptor/expressionList.js
+++ js/scriptor/expressionList.js
@@ -2,16 +2,11 @@ import Explain from './Explain';
 import Timeout from './Timeout';
 import PressAnyKey from './PressAnyKey';
 import ClickAnyWhere from './ClickAnyWhere';
-import Or from './Or';
-import And from './And';
 import Earliest from './Earliest';
 import Latest from './Latest';
 import ShowSnackBar from './ShowSnackBar';
 import Appear from "./Appear";
-import Alternate from "./Alternate";
-import Declare from "./Declare";
 import Var from "./Var";
-import Assign from "./Assign";
 import QuerySelector from "./QuerySelector";
 import UserClick from "./UserClick";
 import CurrentInputText from "./CurrentInputText";
@@ -23,20 +18,15 @@ import ShowToastMessage from "./ShowToastMessage";
 
 var commandList = [
     SetRootView,
-    Declare,
     Var,
-    Assign,
     Explain,
     Timeout,
     PressAnyKey,
     ClickAnyWhere,
-    Or,
-    And,
     Earliest,
     Latest,
     ShowSnackBar,
     Appear,
-    Alternate,
     QuerySelector,
     UserClick,
     UserCheckbox,
