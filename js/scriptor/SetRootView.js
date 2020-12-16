dc685c4c5927eebf46d12a46b3d965d8d901f8c3
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-16 11:21:09 +0700

    - shorter code

--- js/scriptor/SetRootView.js
+++ js/scriptor/SetRootView.js
@@ -1,29 +1,20 @@
-import BaseCommand from './BaseCommand';
-import OOP from 'absol/src/HTML5/OOP';
 import {isDomNode} from "absol/src/HTML5/Dom";
-import FunctionNameManager from "./FunctionNameManager";
+import FunctionNameManager from "./TutorNameManager";
 import {$} from "../dom/Core";
 
-/***
- * @extends {BaseCommand}
- */
-function SetRootView() {
-    BaseCommand.apply(this, arguments);
-}
 
-OOP.mixClass(SetRootView, BaseCommand);
-
+var SetRootView = {};
 
 SetRootView.attachEnv = function (tutor, env) {
-    env.setRootView = function (eltPath){
-        if (typeof  eltPath === 'string'){
+    env.setRootView = function (eltPath) {
+        if (typeof eltPath === 'string') {
             var elt = tutor.findNode(eltPath, true) || $(eltPath);
             tutor.$view = elt || document.body;
         }
         else if (isDomNode(eltPath)) {
             tutor.$view = eltPath;
         }
-        else{
+        else {
             tutor.$view = document.body;
         }
     };
