4f56885de4b4845105a1c1d065580a0a9e66a87a
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-16 11:15:01 +0700

    - shorter code

--- js/scriptor/QuerySelector.js
+++ js/scriptor/QuerySelector.js
@@ -1,30 +1,14 @@
-import BaseCommand from "./BaseCommand";
-import OOP from "absol/src/HTML5/OOP";
 import {$} from "../dom/Core";
-import FunctionNameManager from "./FunctionNameManager";
+import FunctionNameManager from "./TutorNameManager";
 
-/***
- * @extends BaseCommand
- * @constructor
- */
-function QuerySelector() {
-    BaseCommand.apply(this, arguments);
-
-}
-
-OOP.mixClass(QuerySelector, BaseCommand);
-
-QuerySelector.prototype.exec = function () {
-    var elt = $(this.args.query, this.tutor.$view);
-    if (!elt) throw new Error('Can not query element \"'+ this.args.query+'"');
-    return Promise.resolve(elt);
-};
+var  QuerySelector =  {};
 
 QuerySelector.attachEnv = function (tutor, env) {
-    env.querySelector = function (query){
-        return new QuerySelector(tutor, {query: query}).exec();
+    env.querySelector = function (query, root){
+        var elt = $(query, root || tutor.$view);
+        if (!elt) throw new Error('Can not query element \"'+ query+'"');
     };
-    env.$ = $;
+    env.$ = env.querySelector;
 };
 
 
