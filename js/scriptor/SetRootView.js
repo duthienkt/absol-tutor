d21248b467185963716524bffd63c8e34021008e
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-17 16:55:13 +0700

    - setRootView

--- js/scriptor/SetRootView.js
+++ js/scriptor/SetRootView.js
@@ -1,22 +1,12 @@
-import {isDomNode} from "absol/src/HTML5/Dom";
 import FunctionNameManager from "./TutorNameManager";
-import {$} from "../dom/Core";
+import findNode from "../util/findNode";
 
 
 var SetRootView = {};
 
 SetRootView.attachEnv = function (tutor, env) {
     env.setRootView = function (eltPath) {
-        if (typeof eltPath === 'string') {
-            var elt = tutor.findNode(eltPath, true) || $(eltPath);
-            tutor.$view = elt || document.body;
-        }
-        else if (isDomNode(eltPath)) {
-            tutor.$view = eltPath;
-        }
-        else {
-            tutor.$view = document.body;
-        }
+        tutor.$view = findNode(eltPath) || document.body;
     };
 };
 
