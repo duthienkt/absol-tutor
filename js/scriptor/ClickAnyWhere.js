4a1ecec2f35ee2b874050043cc438a445fed98b8
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-27 12:53:45 +0700

    - stop ClickAnyWhere

--- js/scriptor/ClickAnyWhere.js
+++ js/scriptor/ClickAnyWhere.js
@@ -7,7 +7,6 @@ import TutorNameManager from "./TutorNameManager";
  */
 function ClickAnyWhere() {
     BaseCommand.apply(this, arguments);
-    this._isRunning = false;
     this.ev_click = this.ev_click.bind(this);
     this._resolveCb = null;
 }
@@ -16,20 +15,16 @@ OOP.mixClass(ClickAnyWhere, BaseCommand);
 
 
 ClickAnyWhere.prototype.exec = function () {
-    if (this._isRunning) throw new Error("Violation exec!");
-    this._isRunning = true;
+   this.start();
     document.addEventListener('click', this.ev_click);
     return new Promise(function (rs) {
         this._resolveCb = rs;
-    }.bind(this)).then(function () {
-        this.cancel();
-    }.bind(this));
+    }.bind(this)).then(this.stop.bind(this));
 
 };
 
 ClickAnyWhere.prototype.cancel = function () {
     document.removeEventListener('click', this.ev_click);
-    this._isRunning = false;
 };
 
 
