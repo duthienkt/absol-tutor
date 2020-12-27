141e73e73fbfaceb980f702a54b8d09c2002ff25
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-27 14:25:23 +0700

    * cancel if error

--- js/scriptor/ShowToastMessage.js
+++ js/scriptor/ShowToastMessage.js
@@ -15,40 +15,49 @@ function ShowToastMessage() {
 OOP.mixClass(ShowToastMessage, BaseCommand);
 
 ShowToastMessage.prototype.exec = function () {
-    var thisC = this;
-    var title = this.args.title;
-    var text = this.args.text;
-    var until = this.args.until;
-    var variant = this.args.variant;
-    var disappearTimeout = this.args.disappearTimeout;
-    this.start();
-    this.$toast = Toast.make({
-        class: ['as-variant-background', 'atr-toast-message'],
-        props: {
-            htitle: title,
-            disappearTimeout: disappearTimeout,
-            variant: variant,
-            timeText: ''
-        },
-        child: BaseCommand.prototype.md2HTMLElements.call({ $htmlRender: BaseCommand.prototype.$htmlRender }, text)
-    });
-    if (typeof until === "function"){
-        thisC.preventInteract(true);
-        return until().then(function (){
-            this.stop();
+    try {
+        var thisC = this;
+        var title = this.args.title;
+        var text = this.args.text;
+        var until = this.args.until;
+        var variant = this.args.variant;
+        var disappearTimeout = this.args.disappearTimeout;
+        this.start();
+        this.$toast = Toast.make({
+            class: ['as-variant-background', 'atr-toast-message'],
+            props: {
+                htitle: title,
+                disappearTimeout: disappearTimeout,
+                variant: variant,
+                timeText: ''
+            },
+            child: BaseCommand.prototype.md2HTMLElements.call({ $htmlRender: BaseCommand.prototype.$htmlRender }, text)
         });
-    }
-    if (until && until.exec && until.depthClone) {
-        thisC.preventInteract(true);
-        return until.depthClone().exec().then(function (){
-            thisC.stop();
-        });
+        if (typeof until === "function") {
+            thisC.preventInteract(true);
+            return until().then(function () {
+                thisC.$toast = null;
+                this.stop();
+            });
+        }
+        else if (until && until.exec && until.depthClone) {
+            thisC.preventInteract(true);
+            return until.depthClone().exec().then(function () {
+                thisC.$toast = null;
+                thisC.stop();
+            });
+        }
+        else {
+            thisC.$toast = null;
+            // release toast
+        }
+    } catch (err) {
+        console.error(err);
     }
 };
 
-ShowToastMessage.prototype.cancel = function (){
-    console.log("cancel")
-    if (this.$toast){
+ShowToastMessage.prototype.cancel = function () {
+    if (this.$toast) {
         this.$toast.remove();
         this.$toast = null;
     }
