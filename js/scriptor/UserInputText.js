c2df2925bd01bffc8974fe9725cc15447dcc8088
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-28 16:00:37 +0700

    - stop UserInputText

--- js/scriptor/UserInputText.js
+++ js/scriptor/UserInputText.js
@@ -9,6 +9,7 @@ import FunctionKeyManager from "./TutorNameManager";
  */
 function UserInputText() {
     BaseCommand.apply(this, arguments);
+    this._rejectCb = null;
 }
 
 OOP.mixClass(UserInputText, BaseCommand);
@@ -17,77 +18,83 @@ UserInputText.prototype.exec = function () {
     this.start();
     var matchExpression = this.args.match;
     var thisC = this;
-    return Promise.all([
-        this.asyncGetElt(this.args.eltPath),
-        wrapAsync(this.args.message),
-        wrapAsync(this.args.wrongMessage)
-    ]).then(function (args) {
-        var elt = args[0];
-        var message = args[1];
-        var wrongMessage = args[2];
+    var elt = this.tutor.findNode(this.args.eltPath);
+    var message = this.args.message;
+    var wrongMessage = this.args.wrongMessage;
+    var match = this.args.match;
 
-        thisC.tutor.memory.share.getCurrentInputText = function () {
-            return elt.value || elt.text || '';
+    thisC.showToast(message);
+    var changed = false;
+    thisC.onlyInteractWith(elt, function () {
+        var result = verify();
+        if (!result || !changed) {
+            thisC.highlightElt(elt);
         }
-
-        thisC.showToast(message);
-        var changed = false;
-        thisC.onlyInteractWith(elt, function () {
-            verify().then(function (result) {
-                if (!result || !changed) {
-                    thisC.highlightElt(elt);
-                }
-            });
-        });
-
-        function verify() {
-            var isMatchedAsync;
-            if (matchExpression && matchExpression.test) {
-                isMatchedAsync = wrapAsync(!!matchExpression.test(elt.value || elt.text || ''));
-            }
-            else {
-                isMatchedAsync = wrapAsync(matchExpression && matchExpression.depthClone && matchExpression.depthClone());
-            }
-            return isMatchedAsync.then(function (matched) {
-                if (!matched && wrongMessage) {
-                    thisC.showTooltip(elt, wrongMessage);
-                }
-                else if (matched) {
-                    thisC.closeTooltip();
-                }
-                return matched;
-            });
-        }
-
-
-        return new Promise(function (resolve) {
-            function onChange() {
-                verify().then(function (result) {
-                    if (result) {
-                        elt.off('keyup', verify)
-                            .off('change', onChange)
-                            .off('blur', onChange);
-                        thisC.tutor.memory.share.getCurrentInputText = null;
-                        resolve();
-                    }
-                });
-            }
-
-            elt.on('keyup', verify)
-                .on('change', onChange)
-                .on('blur', onChange)
-                .once('change', function () {
-                    changed = true;
-                })
-                .once('blur', function () {
-                    changed = true;
-                });
-        });
-
-    }).then(function () {
-        thisC.stop();
     });
+
+    function verify() {
+        var isMatched;
+        var text = elt.value || elt.text || '';
+        if (matchExpression && matchExpression.test) {
+            isMatched = matchExpression.test(elt.value || elt.text || '');
+        }
+        else if (typeof matchExpression === "function") {
+            isMatched = matchExpression(text);
+        }
+        else if (typeof matchExpression === 'string') {
+            isMatched = text === matchExpression;
+        }
+        else if (typeof matchExpression === 'number') {
+            isMatched = parseFloat(text) === matchExpression;
+        }
+        else {
+            isMatched = text > 0;
+        }
+        if (!isMatched && wrongMessage) {
+            thisC.showTooltip(elt, wrongMessage);
+        }
+        else if (isMatched) {
+            thisC.closeTooltip();
+        }
+        return isMatched;
+    }
+
+
+    return new Promise(function (resolve) {
+        function onChange() {
+            if (verify()) {
+                elt.off('keyup', verify)
+                    .off('change', onChange)
+                    .off('blur', onChange);
+                thisC._rejectCb = null;
+                resolve();
+            }
+        }
+
+        elt.on('keyup', verify)
+            .on('change', onChange)
+            .on('blur', onChange)
+            .once('change', function () {
+                changed = true;
+            })
+            .once('blur', function () {
+                changed = true;
+            });
+        thisC._rejectCb = function () {
+            elt.off('keyup', verify)
+                .off('change', onChange)
+                .off('blur', onChange);
+        };
+    }).then(this.stop.bind(this));
 };
+
+UserInputText.prototype.cancel = function () {
+    if (this._rejectCb) {
+        this._rejectCb();
+        this._rejectCb = null;
+    }
+};
+
 
 UserInputText.attachEnv = function (tutor, env) {
     env.USER_INPUT_TEXT = function (eltPath, match, message, wrongMessage) {
