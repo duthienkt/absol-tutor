d15fe5c871c44cdcf1f3976a60e96bb39d389268
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-12-28 11:03:07 +0700

    - remove try catch

--- js/scriptor/ShowToastMessage.js
+++ js/scriptor/ShowToastMessage.js
@@ -15,44 +15,39 @@ function ShowToastMessage() {
 OOP.mixClass(ShowToastMessage, BaseCommand);
 
 ShowToastMessage.prototype.exec = function () {
-    try {
-        var thisC = this;
-        var title = this.args.title;
-        var text = this.args.text;
-        var until = this.args.until;
-        var variant = this.args.variant;
-        var disappearTimeout = this.args.disappearTimeout;
-        this.start();
-        this.$toast = Toast.make({
-            class: ['as-variant-background', 'atr-toast-message'],
-            props: {
-                htitle: title,
-                disappearTimeout: disappearTimeout,
-                variant: variant,
-                timeText: ''
-            },
-            child: BaseCommand.prototype.md2HTMLElements.call({ $htmlRender: BaseCommand.prototype.$htmlRender }, text)
-        });
-        if (typeof until === "function") {
-            thisC.preventInteract(true);
-            return until().then(function () {
-                thisC.$toast = null;
-                this.stop();
-            });
-        }
-        else if (until && until.exec && until.depthClone) {
-            thisC.preventInteract(true);
-            return until.depthClone().exec().then(function () {
-                thisC.$toast = null;
-                thisC.stop();
-            });
-        }
-        else {
+    var thisC = this;
+    var title = this.args.title;
+    var text = this.args.text;
+    var until = this.args.until;
+    var variant = this.args.variant;
+    var disappearTimeout = this.args.disappearTimeout;
+    this.start();
+    this.$toast = Toast.make({
+        class: ['as-variant-background', 'atr-toast-message'],
+        props: {
+            htitle: title,
+            disappearTimeout: disappearTimeout,
+            variant: variant,
+            timeText: ''
+        },
+        child: BaseCommand.prototype.md2HTMLElements.call({ $htmlRender: BaseCommand.prototype.$htmlRender }, text)
+    });
+    if (typeof until === "function") {
+        thisC.preventInteract(true);
+        return until().then(function () {
             thisC.$toast = null;
-            // release toast
-        }
-    } catch (err) {
-        console.error(err);
+            this.stop();
+        });
+    }
+    else if (until && until.exec && until.depthClone) {
+        thisC.preventInteract(true);
+        return until.depthClone().exec().then(function () {
+            thisC.$toast = null;
+            thisC.stop();
+        });
+    }
+    else {
+        thisC.$toast = null;
     }
 };
 
