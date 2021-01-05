3a2882f2c64dcbe171c53b0294d97ca5e04d6be3
Pham Quoc Du Thien <blueskythien2010@live.com>
2021-01-05 18:04:39 +0700

    - show icon

--- js/scriptor/UserScrollIfNeed.js
+++ js/scriptor/UserScrollIfNeed.js
@@ -3,6 +3,8 @@ import OOP from "absol/src/HTML5/OOP";
 import {traceOutBoundingClientRect} from "absol/src/HTML5/Dom";
 import TACData from "./TACData";
 import TutorNameManager from "./TutorNameManager";
+import {$, _} from "../dom/Core";
+import {ScrollBarIco} from "../dom/Icon";
 
 /***
  * @extends BaseCommand
@@ -15,6 +17,45 @@ function UserScrollIfNeed() {
 
 OOP.mixClass(UserScrollIfNeed, BaseCommand);
 
+UserScrollIfNeed.prototype.$scrollBarIcon = $(ScrollBarIco.cloneNode(true));
+UserScrollIfNeed.prototype.$scrollBarIconCtn = _({
+    class: 'atr-scroll-icon-ctn',
+    child: UserScrollIfNeed.prototype.$scrollBarIcon
+})
+
+UserScrollIfNeed.prototype._showScroll = function (elt, dir) {
+    console.log(elt, dir)
+    this.$scrollBarIcon.removeClass('atr-down')
+        .removeClass('atr-up')
+        .removeStyle('transform');
+
+    this.$scrollBarIconCtn.removeStyle('left')
+        .removeStyle('top');
+    if (elt && !this.$scrollBarIconCtn.parentElement)
+        document.body.appendChild(this.$scrollBarIconCtn);
+    else if (!elt && this.$scrollBarIconCtn.parentElement) {
+        this.$scrollBarIconCtn.remove();
+        return;
+    }
+    if (!dir) return;
+    var bound = elt.getBoundingClientRect();
+    var iconBound = this.$scrollBarIconCtn.getBoundingClientRect();
+    if (dir.dy) {
+        this.$scrollBarIconCtn.addStyle('left', bound.right - 17 -iconBound.width - 5 + 'px');
+        if (dir.dy > 0) {
+            this.$scrollBarIcon.addClass('atr-up');
+            this.$scrollBarIconCtn.addStyle('top', bound.top + 'px');
+
+        }
+        else {
+            this.$scrollBarIcon.addClass('atr-down');
+            this.$scrollBarIconCtn.addStyle('top', bound.bottom - iconBound.height - 5 + 'px');
+
+        }
+    }
+
+
+};
 
 UserScrollIfNeed.prototype.findVScroller = function (elt, dY) {
     var parent = elt.parentElement;
@@ -49,6 +90,10 @@ UserScrollIfNeed.prototype._findScrollDir = function (elt) {
     return { dx: dx, dy: dy };
 };
 
+UserScrollIfNeed.prototype.onStop = function () {
+    BaseCommand.prototype.onStop.apply(this, arguments);
+};
+
 
 UserScrollIfNeed.prototype.exec = function () {
     this.start();
@@ -67,9 +112,9 @@ UserScrollIfNeed.prototype.exec = function () {
     return new Promise(function (resolve, reject) {
         var checkTimeoutId = -1;
         var currentDir;
-        function check(){
+
+        function check() {
             currentDir = thisC._findScrollDir(elt);
-            console.log(currentDir)
             if ((currentDir.dx === 0 || !hScroller) && (currentDir.dy === 0 || !vScroller)) {
                 thisC._rejectCb = null;
                 if (vScroller) {
@@ -79,8 +124,9 @@ UserScrollIfNeed.prototype.exec = function () {
             }
 
         }
+
         function onScroll(event) {
-            if (checkTimeoutId){
+            if (checkTimeoutId) {
                 clearTimeout(checkTimeoutId);
             }
             currentDir = thisC._findScrollDir(elt);
@@ -89,16 +135,19 @@ UserScrollIfNeed.prototype.exec = function () {
         }
 
         if (vScroller) {
+            thisC._showScroll(vScroller, scrollDir);
+            vScroller.addClass('atr-scroll-only');
             thisC.onlyClickTo(vScroller);
             vScroller.addEventListener('scroll', onScroll)
         }
 
         thisC._rejectCb = function () {
-            if (checkTimeoutId){
+            if (checkTimeoutId) {
                 clearTimeout(checkTimeoutId);
             }
             if (vScroller) {
                 vScroller.removeEventListener('scroll', onScroll);
+                vScroller.removeClass('atr-scroll-only');
             }
             reject();
         }
