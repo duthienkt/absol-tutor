ccd1649eaf9620a45f6943b427f4f2eabf1b4805
duthienkt <blueskythien2010@live.com>
2021-01-29 23:07:57 +0700

    - keyBinding

--- js/dependents.js
+++ js/dependents.js
@@ -2,6 +2,8 @@ import 'absol/src/dependents';
 import 'absol-brace/dependents';
 import 'brace/ext/language_tools';
 import 'brace/snippets/javascript';
+import 'brace/keybinding/emacs';
+import 'brace/ext/searchbox';
 import generator from '@babel/generator';
 import presetEnv from '@babel/preset-env';
 window.babel.generator = generator;
