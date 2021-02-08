27e3b1ad91188ebde6ef0fb1d5412321dedc7488
duthienkt <blueskythien2010@live.com>
2021-02-08 13:51:35 +0700

    - update target

--- js/scriptor/UserBaseAction.js
+++ js/scriptor/UserBaseAction.js
@@ -21,7 +21,17 @@ UserBaseAction.prototype.requestUserAction = function () {
 UserBaseAction.prototype.exec = function () {
     this.start();
     this.assignTarget(this.tutor.findNode(this.args.eltPath, true));
-    return this.showDelayToast(this.args.message).then(this.requestUserAction.bind(this))
+    return this.showDelayToast(this.args.message)
+        .then(function (){
+            var target = this.tutor.findNode(this.args.eltPath, true);
+            this.assignTarget(target);
+            setTimeout(function (){
+                if (this.state === "RUNNING"){
+                    this.assignTarget(target);
+                }
+            }.bind(this),200)
+        }.bind(this))
+        .then(this.requestUserAction.bind(this))
         .then(this.stop.bind(this));
 };
 
