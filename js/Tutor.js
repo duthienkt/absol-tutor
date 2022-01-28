b8e9f98610152a07a60382887c9d34642d38b9d6
duthienkt <blueskythien2010@live.com>
2022-01-28 10:47:42 +0700

    - Tutor

--- js/Tutor.js
+++ js/Tutor.js
@@ -5,6 +5,9 @@ import findAllNode from "./util/findAllNode";
 import Context from "absol/src/AppPattern/Context";
 import OOP from "absol/src/HTML5/OOP";
 import EventEmitter from "absol/src/HTML5/EventEmitter";
+import { stringHashCode } from "absol/src/String/stringUtils";
+import TutorEngine from "./scriptor/TutorEngine";
+import safeThrow from "absol/src/Code/safeThrow";
 
 
 /***
@@ -18,72 +21,34 @@ function Tutor(view, script, option) {
     EventEmitter.call(this);
     Context.call(this);
     this.$view = view;
-    this.script = null;
+    /***
+     *
+     * @type {*|TProgram}
+     */
+    this.prorgram = this.programCache[stringHashCode(script)] || TutorEngine.compile(script);
+    /***
+     *
+     * @type {TProcess}
+     */
+    this.process = this.prorgram.newProcess();
+    this.process.tutor = this;
 
 
     this.option = Object.assign({
         messageDelay: 300
     }, option);
-    this.debug = {
-        status: "NOT_START",
-        loc: {
-            start: -1,
-            end: -1,
-            row: 0, //indexing from 1
-            col: 0,
-        }
-    };
-    this._commandStack = [];
-    this._compile(script);
 }
 
 OOP.mixClass(Tutor, EventEmitter, Context);
 
-Tutor.prototype._compile = function (script) {
-    this.script = buildTutorJavaScript(script);
-};
-
-Tutor.prototype.commandPush = function (command) {
-    if (this._commandStack.indexOf(command) < 0) {
-        this._commandStack.push(command);
-    }
-    else {
-        throw new Error("Command Stack Error: re-push command!");
-    }
-};
-
-
-Tutor.prototype.commandPop = function (command) {
-    if (this._commandStack.indexOf(command) >= 0) {
-        while (this._commandStack.length > 0) {
-            if (this._commandStack.pop() === command) break;
-        }
-    }
-    else {
-        throw new Error("Command Stack Error: pop command not in stack!");
-    }
-};
+Tutor.prototype.programCache = {};
 
 
 Tutor.prototype.exec = function () {
-    this.start();
-    var tutor = this;
-    var env = {};
-    expressionList.forEach(function (exp) {
-        if (exp.attachEnv) {
-            exp.attachEnv(tutor, env);
-        }
-    });
-    var args = this.script.argNames.map(function (name) {
-        return env[name];
-    });
-    this.debug.status = "RUNNING";
-    return this.script.exec.apply(null, args).then(function (result) {
-        this.stop();
-        this.debug.status = "FINISH";
+    return this.process.exec().then(function (result) {
         return result;
     }.bind(this)).catch(function (error) {
-        this.stop();
+        safeThrow(error)
         if (error instanceof Error) {
             this.debug.status = "ERROR";
             throw error;
@@ -91,12 +56,8 @@ Tutor.prototype.exec = function () {
     }.bind(this));
 };
 
-
-Tutor.prototype.onStop = function () {
-    var cmdList = this._commandStack.slice();
-    for (var i = cmdList.length - 1; i >= 0; --i) {
-        cmdList[i].stop();
-    }
+Tutor.prototype.stop = function (){
+    this.process.stop();
 };
 
 
@@ -118,14 +79,22 @@ Tutor.prototype.getStatus = function () {
     if (this.debug.status === "RUNNING" || this.debug.status === "ERROR")
         return {
             status: this.debug.status,
-            code: this.script.code.substr(this.debug.loc.start, this.debug.loc.end - this.debug.loc.start),
+            code: this.process.program.code.substr(this.debug.loc.start, this.debug.loc.end - this.debug.loc.start),
             row: this.debug.loc.row - 1,
             col: this.debug.loc.col
         };
     return {
         status: "FINISH"
     };
-}
+};
+
+Object.defineProperty(Tutor.prototype, 'debug', {
+    get: function (){
+        return this.process.debug;
+    }
+})
+
+
 
 
 export default Tutor;
