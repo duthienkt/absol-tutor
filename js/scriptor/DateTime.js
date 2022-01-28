7ac5141d6cc2548c5478047ec0b31dd5412e7eb4
duthienkt <blueskythien2010@live.com>
2022-01-28 15:01:41 +0700

    - DateTime

--- js/scriptor/DateTime.js
+++ js/scriptor/DateTime.js
@@ -1,15 +1,18 @@
 import TutorNameManager from "./TutorNameManager";
 import {formartDateString, parseDateString} from 'absol/src/Time/datetime';
+import TutorEngine from "./TutorEngine";
+import  * as datetime from 'absol/src/Time/datetime';
 
 var DateTime = {};
 
-DateTime.properties = {};
+DateTime.properties = Object.assign({}, datetime);
 
 DateTime.properties.parseDMY = function (str) {
     return parseDateString(str, 'dd/mm/yyyy');
 };
 
 DateTime.properties.formatDateString = formartDateString;
+
 
 DateTime.libs = {};
 Object.keys(DateTime.properties).forEach(function (key) {
@@ -20,9 +23,7 @@ Object.keys(DateTime.properties).forEach(function (key) {
     });
 });
 
-DateTime.attachEnv = function (tutor, env) {
-    env.datetime = DateTime.libs;
-};
+TutorEngine.installConst('datetime', DateTime.libs);
 
 TutorNameManager.addConst('datetime');
 
