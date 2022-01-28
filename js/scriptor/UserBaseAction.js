523693d6ca333189451cf5c0333a021237d69151
duthienkt <blueskythien2010@live.com>
2022-01-28 10:48:17 +0700

    - UserBaseAction

--- js/scriptor/UserBaseAction.js
+++ js/scriptor/UserBaseAction.js
@@ -1,5 +1,62 @@
 import OOP from "absol/src/HTML5/OOP";
 import BaseCommand from "./BaseCommand";
+import { inheritCommand } from "../engine/TCommand";
+import BaseState from "./BaseState";
+
+
+/***
+ * @extends BaseState
+ * @param {BaseCommand} command
+ * @constructor
+ */
+function StateWaitElt(command) {
+    BaseState.call(this, command);
+    this.checkElt = this.checkElt.bind(this);
+    this.countDown = 20;
+}
+
+
+OOP.mixClass(StateWaitElt, BaseState);
+
+
+StateWaitElt.prototype.onStart = function () {
+    this.inv = setInterval(this.checkElt, 100);
+};
+
+StateWaitElt.prototype.onStop = function () {
+    clearInterval(this.inv);
+}
+
+StateWaitElt.prototype.checkElt = function () {
+    this.command.elt = this.command.findNode(this.args.eltPath, true);
+    this.countDown--;
+    if (this.countDown <= 0) {
+        this.command.reject(new Error("Could not find " + this.args.eltPath));
+        return;
+    }
+    if (this.command.elt) {
+        this.command.assignTarget(this.command.elt);
+        this.goto('show_message');
+    }
+};
+
+/***
+ * @extends BaseState
+ * @param {BaseCommand} command
+ * @constructor
+ */
+function StateShowMessage(command) {
+    BaseState.call(this, command);
+}
+
+OOP.mixClass(StateShowMessage, BaseState);
+
+
+StateShowMessage.prototype.onStart = function () {
+    this.command.showDelayToast(this.args.message).then(function () {
+        this.goto('user_begin');
+    }.bind(this));
+}
 
 /***
  * @extends BaseCommand
@@ -9,7 +66,9 @@ function UserBaseAction() {
     BaseCommand.apply(this, arguments);
 }
 
-OOP.mixClass(UserBaseAction, BaseCommand);
+inheritCommand(UserBaseAction, BaseCommand);
+
+
 /***
  *
  * @return {Promise<void>}
@@ -18,21 +77,9 @@ UserBaseAction.prototype.requestUserAction = function () {
     return Promise.resolve();
 };
 
-UserBaseAction.prototype.exec = function () {
-    this.start();
-    this.assignTarget(this.tutor.findNode(this.args.eltPath, true));
-    return this.showDelayToast(this.args.message)
-        .then(function (){
-            var target = this.tutor.findNode(this.args.eltPath, true);
-            this.assignTarget(target);
-            setTimeout(function (){
-                if (this.state === "RUNNING"){
-                    this.assignTarget(target);
-                }
-            }.bind(this),200)
-        }.bind(this))
-        .then(this.requestUserAction.bind(this))
-        .then(this.stop.bind(this));
-};
+UserBaseAction.prototype.stateClass.entry = StateWaitElt;
+UserBaseAction.prototype.stateClass.show_message = StateShowMessage;
+UserBaseAction.prototype.stateClass.user_begin = UserBaseAction.prototype.stateClass.finish;
+
 
 export default UserBaseAction;
