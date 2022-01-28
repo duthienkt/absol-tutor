9898e41b9f0697a12f8999a686888478ad76d088
duthienkt <blueskythien2010@live.com>
2022-01-28 10:49:06 +0700

    - Appear

--- js/scriptor/Appear.js
+++ js/scriptor/Appear.js
@@ -1,48 +1,80 @@
 import BaseCommand from './BaseCommand';
 import OOP from 'absol/src/HTML5/OOP';
-import {$} from "../dom/Core";
+import { $ } from "../dom/Core";
+import { inheritCommand } from "../engine/TCommand";
+import BaseState from "./BaseState";
+import TutorEngine from "./TutorEngine";
+
+/***
+ * @extends BaseState
+ * @constructor
+ */
+function StateWaitElt() {
+    BaseState.apply(this, arguments);
+    this.timeOut = -1;
+}
+
+OOP.mixClass(StateWaitElt, BaseState);
+
+StateWaitElt.prototype.onStart = function () {
+    this.endTime = new Date().getTime() + (this.args.timeout || 100);
+    this.checkElt();
+};
+
+StateWaitElt.prototype.onStop = function () {
+    if (this.timeOut >= 0) {
+        clearTimeout(this.timeOut);
+        this.timeOut = -1;
+    }
+
+};
+
+
+StateWaitElt.prototype.isVisibility = function (elt) {
+    var eltBound;
+    if (elt) {
+        eltBound = elt.getBoundingClientRect();
+        if (eltBound.width > 0 || eltBound.height > 0) {
+            if ($(elt).getComputedStyleValue('visibility') !== 'hidden') {
+                return true;
+            }
+        }
+
+    }
+    return false;
+};
+
+StateWaitElt.prototype.checkElt = function () {
+    var now = new Date().getTime();
+    this.timeOut = -1;
+    var elt = this.command.findNode(this.args.eltPath, true);
+    if (this.isVisibility(elt)) {
+        this.command.resolve(true);
+    }
+    else if (now > this.endTime) {
+        this.command.resolve(false);
+    }
+    else {
+        this.timeOut = setTimeout(this.checkElt.bind(this), Math.min(100, this.endTime - now));
+    }
+};
+
 
 /***
  * @extends {BaseCommand}
+ * @constructor
  */
 function Appear() {
     BaseCommand.apply(this, arguments);
 }
 
-OOP.mixClass(Appear, BaseCommand);
+inheritCommand(Appear, BaseCommand);
 
-Appear.prototype.exec = function () {
-    var tutor = this.tutor;
-    var eltPath = this.args.eltPath;
-    return new Promise(function (resolve) {
-        var elt;
-        var timeoutIdx = setTimeout(function () {
-            resolve(false);
-            clearInterval(intervalIdx);
-        }.bind(this));
-        var eltBound;
-        var intervalIdx = setInterval(function () {
-            if (!elt) {
-                elt = tutor.findNode(eltPath, true) || $(eltPath);
-            }
-            if (elt) {
-                eltBound = elt.getBoundingClientRect();
-                if (eltBound.width > 0 || eltBound.height > 0) {
-                    if ($(eltBound).getComputedStyleValue('visibility') !== 'hidden') {
-                        clearTimeout(timeoutIdx);
-                        clearInterval(intervalIdx);
-                        resolve(true);
-                    }
-                }
-            }
-        }, 200);
-    });
-};
+Appear.prototype.className = 'Appear';
+Appear.prototype.argNames = ['eltPath', 'timeout'];
+Appear.prototype.name = 'APPEAR';
+Appear.prototype.stateClass.entry = StateWaitElt;
 
-Appear.attachEnv = function (tutor, env) {
-    env.APPEAR = function (eltPath, timeout) {
-        return new Appear(tutor, { eltPath: eltPath, timeout: timeout || 100000 });
-    };
-};
+TutorEngine.installCommand(Appear);
 
 export default Appear;
