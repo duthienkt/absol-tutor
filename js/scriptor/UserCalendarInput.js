3c84e3c453a206eccc12cc01b9abdcbdac91aa9d
duthienkt <blueskythien2010@live.com>
2022-02-06 14:05:21 +0700

    - UserCalendarInput

--- js/scriptor/UserCalendarInput.js
+++ js/scriptor/UserCalendarInput.js
@@ -1,9 +1,87 @@
 import OOP from "absol/src/HTML5/OOP";
 import TutorNameManager from "./TutorNameManager";
-import {compareDate} from "absol/src/Time/datetime";
 import ChromeCalendar from "absol-acomp/js/ChromeCalendar";
 import TACData from "./TACData";
 import UserBaseAction from "./UserBaseAction";
+import { inheritCommand } from "../engine/TCommand";
+import BaseState from "./BaseState";
+import TutorEngine from "./TutorEngine";
+import { StateCheckDateValue, StateChooseDate } from "./UserDateInput";
+
+/***
+ * @extends BaseState
+ * @constructor
+ */
+function StateWaitCalendar() {
+    BaseState.apply(this, arguments);
+    this.clickTO = -1;
+}
+
+OOP.mixClass(StateWaitCalendar, BaseState);
+
+StateWaitCalendar.prototype.onStart = function () {
+    var elt = this.command.elt;
+    var thisC = this.command;
+    thisC.onlyClickTo(elt);
+    thisC.highlightElt(elt);
+    if (thisC.hadWrongAction && this.args.wrongMessage) {
+        thisC.showTooltip(elt, thisC.args.wrongMessage);
+    }
+
+    document.addEventListener('click', this.ev_click);
+};
+
+StateWaitCalendar.prototype.onStop = function () {
+    clearTimeout(this.clickTO);
+    document.removeEventListener('click', this.ev_click);
+};
+
+
+StateWaitCalendar.prototype.ev_click = function () {
+    var thisC = this.command;
+    var elt = this.command.elt;
+    thisC.highlightElt(null);
+    if (this.clickTO >= 0) clearTimeout(this.clickTO);
+    this.clickTO = setTimeout(function () {
+        this.clickTO = -1;
+        if (ChromeCalendar.$calendar && ChromeCalendar.$calendar.isDescendantOf(document.body)) {
+            this.goto('choose_date');
+
+        }
+    }.bind(this), 100);
+};
+
+/***
+ * @extends StateChooseDate
+ * @constructor
+ */
+function StateChooseDate1() {
+    StateChooseDate.apply(this, arguments);
+    this.clickTO = -1;
+}
+
+OOP.mixClass(StateChooseDate1, StateChooseDate);
+
+StateChooseDate1.prototype.onStart = function () {
+    this.calendarElt = ChromeCalendar.$calendar;
+    this.command.highlightElt(this.calendarElt);
+    this.blindIdx = setTimeout(this.highlightTarget.bind(this), 400);
+    this.command.onlyClickTo(this.calendarElt);
+    document.addEventListener('click', this.ev_click);
+};
+
+
+/***
+ * @extends BaseState
+ * @constructor
+ */
+function StateWaitChange() {
+    BaseState.apply(this, arguments);
+}
+
+OOP.mixClass(StateWaitChange, BaseState);
+
+
 
 
 /***
@@ -12,114 +90,27 @@ import UserBaseAction from "./UserBaseAction";
  */
 function UserCalendarInput() {
     UserBaseAction.apply(this, arguments);
-    this._rejectCb = null;
 }
 
-OOP.mixClass(UserCalendarInput, UserBaseAction);
+inheritCommand(UserCalendarInput, UserBaseAction);
 
-UserCalendarInput.prototype._afterSelectCalendar = function (elt, requestValue, highlight) {
-    var thisC = this;
-    this.onlyClickTo(elt);
-    thisC.highlightElt(elt);
-    return new Promise(function (resolve, reject) {
-        if (highlight) {
-            if (thisC.args.wrongMessage) {
-                thisC.showTooltip(elt, thisC.args.wrongMessage);
-            }
-        }
+UserCalendarInput.prototype.className = 'UserCalendarInput';
+UserCalendarInput.prototype.name = 'userCalendarInput';
+UserCalendarInput.prototype.argNames = ['eltPath', 'value', 'message', 'wrongMessage'];
+UserCalendarInput.prototype.stateClasses['user_begin'] = StateWaitCalendar;
+UserCalendarInput.prototype.stateClasses['choose_date'] = StateChooseDate1;
+UserCalendarInput.prototype.stateClasses['check_value'] = StateCheckDateValue;
 
-        var clickTimeout = -1;
 
-        function onCLick() {
-            if (clickTimeout >= 0) clearTimeout(clickTimeout);
-            thisC.highlightElt(null);
-            clickTimeout = setTimeout(function () {
-                clickTimeout = -1;
-                if (ChromeCalendar.$calendar && ChromeCalendar.$calendar.isDescendantOf(document.body)) {
-                    thisC.onlyClickTo(ChromeCalendar.$calendar);
-                    if (highlight) {
-                        if (thisC.args.wrongMessage) {
-                            thisC.showTooltip(ChromeCalendar.$calendar, thisC.args.wrongMessage);
-                        }
-                    }
-                }
-                else {
-                    highlight = true;
-                    thisC.onlyClickTo(elt);
-                    thisC.highlightElt(elt);
-                    if (thisC.args.wrongMessage) {
-                        thisC.showTooltip(elt, thisC.args.wrongMessage);
-                    }
-
-                }
-            }, 30);
-        }
-
-        function onChange(event) {
-            var value = event.value;
-            if (clickTimeout >= 0) {
-                clearTimeout(clickTimeout);
-                clickTimeout = -1;
-            }
-            if (compareDate(value, requestValue) === 0) {
-                document.body.removeEventListener('click', onCLick);
-                elt.off('change', onChange);
-                setTimeout(function () {
-                    thisC._rejectCb = null;
-                    resolve(true);
-                }, 10);
-            }
-            else {
-                highlight = true;
-                thisC.onlyClickTo(elt);
-                thisC.highlightElt(elt);
-                if (thisC.args.wrongMessage) {
-                    thisC.showTooltip(elt, thisC.args.wrongMessage);
-                }
-            }
-        }
-
-        elt.on('change', onChange);
-        setTimeout(function () {
-            document.body.addEventListener('click', onCLick);
-        }, 10);
-        thisC._rejectCb = function () {
-            if (clickTimeout >= 0) {
-                clearTimeout(clickTimeout);
-                clickTimeout = -1;
-            }
-            elt.off('change', onChange);
-            document.body.removeEventListener('click', onCLick);
-            reject();
-        };
-    });
-};
-
-UserCalendarInput.prototype._verityCalendarInput = function (elt) {
-    if (!elt.containsClass || !elt.containsClass('absol-calendar-input')){
-        throw new Error("Invalid element type: not a CalendarInput!");
+UserCalendarInput.prototype.verifyElt = function () {
+    var elt = this.elt;
+    if (!elt.hasClass || !elt.hasClass('absol-calendar-input')) {
+        return new Error("Invalid element type: not a CalendarInput!");
     }
+    return null;
 };
 
-UserCalendarInput.prototype.requestUserAction = function () {
-    var thisC = this;
-    var elt = thisC.tutor.findNode(thisC.args.eltPath);
-    this._verityCalendarInput(elt);
-    var value = this.args.value;
-    return this._afterSelectCalendar(elt, value);
-};
-
-
-UserCalendarInput.attachEnv = function (tutor, env) {
-    env.userCalendarInput = function (eltPath, value, message, wrongMessage) {
-        return new UserCalendarInput(tutor, {
-            eltPath: eltPath,
-            value: new Date(value),
-            message: message,
-            wrongMessage: wrongMessage
-        }).exec();
-    };
-};
+TutorEngine.installClass(UserCalendarInput);
 
 TutorNameManager.addAsync('userCalendarInput');
 
