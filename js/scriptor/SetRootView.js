523446019f492864bacce8673bbbc04bd3cff9e6
duthienkt <blueskythien2010@live.com>
2022-08-18 23:19:15 +0700

    - setRootView function

--- js/scriptor/SetRootView.js
+++ js/scriptor/SetRootView.js
@@ -1,14 +1,23 @@
 import FunctionNameManager from "./TutorNameManager";
 import findNode from "../util/findNode";
+import TutorEngine from "./TutorEngine";
 
 
-var SetRootView = {};
+var SetRootView = {
+    prototype: {
+        name: 'setRootView',
+        type: 'sync'
+    }
+};
 
 SetRootView.attachEnv = function (tutor, env) {
     env.setRootView = function (eltPath) {
         tutor.$view = findNode(eltPath) || document.body;
     };
 };
+
+TutorEngine.installClass(SetRootView);
+
 
 FunctionNameManager.addSync('setRootView');
 
